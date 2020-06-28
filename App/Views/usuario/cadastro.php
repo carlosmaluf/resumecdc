aec1b3f5cd42797ed3ab63538cbfe9e4125d00b8
carlosmaluf <carlosagmaluf@outlook.com>
2020-06-28 19:22:50 -0300

    Add files via upload

--- App/Views/usuario/cadastro.php
+++ App/Views/usuario/cadastro.php
@@ -1,1 +1,26 @@
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <h3>Entre com seu nome e sua senha</h3>
+
+            <?php if($Sessao::retornaMensagem()){ ?>
+                <div class="alert alert-warning" role="alert"><?php echo $Sessao::retornaMensagem(); ?></div>
+            <?php } ?>
+
+            <form action="http://<?php echo APP_HOST; ?>/usuario/validacao" method="post" id="form_cadastro">
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control"  name="nome" placeholder="Seu nome" value="<?php echo $Sessao::retornaValorFormulario('nome'); ?>" required>
+                </div>
+                <div class="form-group">
+                    <label for="senha">Senha</label>
+                    <input type="text" class="form-control" name="senha" placeholder="Sua senha" value="<?php echo $Sessao::retornaValorFormulario('senha'); ?>" required>
+                </div>
+                <button type="submit" class="btn btn-success btn-sm">Entrar</button>
+            </form>
+        </div>
+        <div class=" col-md-3"></div>
+    </div>
+</div>
